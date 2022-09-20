78c5beaf03de73d15ead2050f51cc86895be439f
gabrysera <gabrieleserafini10100@gmail.com>
2022-09-20 15:31:02 +0200

    added player controller

---
+++ four-in-a-row-python/src/NRow/Players/PlayerController.py
@@ -0,0 +1,29 @@
+import abc
+from NRow.Board import *
+from NRow.Heuristic import *
+
+from __future__ import annotations
+
+class PlayerController(abc.ABCMeta):
+    
+    #constructor
+    def __init__(self, playerId:int, gameN:int, heuristic:Heuristic):
+        self.playerId = playerId
+        self.gameN = gameN
+        self.heuristic = heuristic
+
+    #how many time the heuristic was used to evaluate a boardstate
+    def getEvalCount(self):
+        return Heuristic.getEvalCount()
+
+    #string representation of player for board
+    def toString(self):
+        if self.playerId == 2:
+            return "O"
+        else:
+            return "X"
+
+    #abstract method for player classes
+    @abc.abstractmethod
+    def makeMove(self, board:Board) -> int:
+        pass
