e11b2261251c451cb4dfb208572d7c0e0dfde04e
gabrysera <gabrieleserafini10100@gmail.com>
2022-09-20 15:13:01 +0200

    board class

---
+++ four-in-a-row-python/src/NRow/Board.py
@@ -0,0 +1,70 @@
+from __future__ import annotations
+
+class Board:
+
+    #constructor
+    def __init__(self, width:int = 0, height:int = 0, board:Board = None, boardState = [[]]):
+        if board is not None:
+            self.__setBoardParameters(board.width, board.height, board.boardState)
+        elif boardState != [[]]:
+            self.__setBoardParameters(len(boardState), len(boardState[0]), boardState)
+        else:
+            self.__setBoardParameters(width, height, [[0 for x in range(height)] for y in range(width)])
+    
+    #function used to abstract different constructors
+    def __setBoardParameters(self, width:int = 0, height:int = 0, boardState = [[]]):
+        self.width = width
+        self.height = height
+        self.boardState = boardState
+
+    #get value in a certain coordinate
+    def getValue(self, x:int, y:int) -> int:
+        return self.boardState[x][y]
+
+    #get a copy of the state
+    def getBoardState(self) -> list(list(int)):
+        return [row[:] for row in self.boardState]
+
+    #player ID make a move in column x
+    def play(self, x:int, playerId:int) -> bool:
+        for i in range(len(self.getBoardState[0])-1, 0, -1): #check column starting from the bottom
+            if self.boardState[x][i] == 0:
+                self.boardState[x][i] = playerId
+            return True
+        return False
+
+    #check if a move is valid
+    def isValid(self, x:int) -> bool:
+        return self.boardState[x][0] == 0
+
+    #Gets a new board given a player and their action (WHY DO WE NEED THIS?)
+    def getNewBoard(self, x:int, playerId:int) -> Board:
+        newBoardState = self.getBoardState()
+        for i in range(len(newBoardState[0])-1, 0, -1): #check column starting from the bottom
+            if newBoardState[x][i] == 0:
+                self.boardState[x][i] = playerId
+                return Board(boardState = newBoardState) #maybe put a break instead of double return
+        return Board(boardState = newBoardState)
+
+    #print board
+    def printBoard(self):
+        divider, divider2 = " "
+        numberRow = "|"
+        output = ""
+        for i in range(0, len(self.boardState)):
+            divider += "--- "
+            divider2 += "==="
+            numberRow += " " + (str(i + 1)) + " |"
+        
+        for i in range(0, len(self.boardState[0])):
+            output += "\n" + divider + "\n"
+            for j in range(0, len(self.boardState)):
+                node = " "
+                if self.boardState[j][i] == 1:
+                    node = "X"
+                elif self.boardState[j][i] == 2:
+                    node = "0"
+                output += "| " + node + " "
+            output += "|"
+        output += "\n" + divider2 + "\n" + numberRow + "\n"
+        print(output)
