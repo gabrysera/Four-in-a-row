2af968d392b7563d996f3bba3dae22436faf247f
gabrysera <gabrieleserafini10100@gmail.com>
2022-09-21 10:20:20 +0200

    game file finished, app started

---
+++ four-in-a-row-python/src/NRow/App.py
@@ -0,0 +1,18 @@
+from NRow.Heuristic import SimpleHeuristic
+from NRow.Players import PlayerController
+from NRow.Players import HumanPlayer
+
+class App:
+
+    def main(self):
+        gameN = 4
+        boardWidth = 7
+        boardHeight = 6
+
+        players = self.__getPlayers(n=gameN)
+        
+
+    def __getPlayers(n:int) -> list(PlayerController):
+        pass
+
+    
---
+++ four-in-a-row-python/src/NRow/Game.py
@@ -0,0 +1,93 @@
+from NRow.Players import PlayerController
+import Board
+
+def __getPlayers(n:int) -> list(PlayerController):
+        pass
+
+class Game:
+
+    def __init__(self, gameN:int, boardWidth:int, boardHeight:int, players:list(PlayerController)):
+        assert(boardWidth % 2 != 0)
+        self.gameN = gameN
+        self.players = players
+        self.winner = -1
+        self.gameBoard:Board = Board(boardWidth, boardHeight)
+
+    def startGame(self) -> int:
+        print("start game!")
+        currentPlayer = 0
+
+        while(not self.isOver()):
+            self.gameBoard.play(self.players[currentPlayer].makeMove(self.gameBoard), self.players[currentPlayer].playerId)
+            currentPlayer = 1 if currentPlayer == 0 else 0
+
+        self.gameBoard.printBoard()
+        if self.winner < 0:
+            print("game is a draw")
+        else:
+            print(f"Player {self.players[self.winner-1].__str__()} won!")
+        print(f"Player {self.players[0].__str__()} evaluated the board {self.players[0].getEvalCount()} times")
+        print(f"Player {self.players[1].__str__()} evaluated the board {self.players[1].getEvalCount()} times")
+
+    #function that determine if the game is over
+    def isOver(self) -> bool:
+        self.winner = self.winning(self.gameBoard.getBoardState(), self.gameN)
+        return self.winner != 0
+
+    #determines if a player has won, if so return his playerId
+    def winning(board:list(list(int)), gameN:int) -> int:
+
+        #vertical check
+        for i in range(0, len(board)):
+            for j in range(0, len(board[i])):
+                if(board[i][j] != 0):
+                    player = board[i][j]
+                    for x in range(1, gameN):
+                        if board[i][j+x] != player:
+                            player = 0
+                            break
+                    if player != 0:
+                        return player
+
+        #horizontal check
+        for i in range(0, len(board-gameN+1)):
+            for j in range(0, len(board[i])):
+                if board[i][j] != 0:
+                    player = board[i][j]
+                    for x in range(1,gameN):
+                        if board[i+x][j] != player:
+                            player = 0
+                            break
+                    if player != 0:
+                        return player
+
+        #acending diagonal check
+        for i in range(0, len(board) - gameN - 1):
+            for j in range(len(board[i]) - 1, gameN-1, -1):
+                if board[i][j] != 0:
+                    player = board[i][j]
+                    for x in range(1,gameN):
+                        if board[i+x][j-x] != player:
+                            player = 0
+                            break
+                    if player != 0:
+                        return player
+
+        #descending diagonal check
+        for i in range(0, len(board -gameN + 1)):
+            for j in range(len(board[i])- gameN + 1):
+                if board[i][j] != 0:
+                    player = board[i][j]
+                    for x in range(1,gameN):
+                        if board[i+x][j+x] != player:
+                            player = 0
+                            break
+                    if player != 0:
+                        return player
+
+        #check for a draw
+        for i in range(0,len(board)):
+            if board[i][0] == 0:
+                return 0 #board not full yet
+        return -1 #is a draw
+            
